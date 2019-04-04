06444ca907600909035721e8abf817f54994c418
Stanislas Gorremans <sgorremans@yoda.info.ucl.ac.be>
2019-04-04 10:02:03 +0200

    Creation of a MakeFile with a random v1.c code

---
+++ v1.c
@@ -0,0 +1,21 @@
+int main( int argc, char*argv[]){
+    if(!strcmp(argv[2],"-eq")){
+    	if(atoi(argv[1]) == atoi(argv[3])){return 0;}
+    	return 1;}
+    if(!strcmp(argv[2],"-ge")){
+    	if(atoi(argv[1]) >= atoi(argv[3])){return 0;}
+    	return 1;}       
+    if(!strcmp(argv[2],"-gt")){
+    	if(atoi(argv[1]) > atoi(argv[3])){return 0;}
+    	return 1;}   
+	if(!strcmp(argv[2],"-le")){
+    	if(atoi(argv[1]) <= atoi(argv[3])){return 0;}
+    	return 1;}  
+    if(!strcmp(argv[2],"-lt")){
+    	if(atoi(argv[1]) < atoi(argv[3])){return 0;}
+    	return 1;}  
+    if(!strcmp(argv[2],"-ne")){
+    	if(atoi(argv[1]) != atoi(argv[3])){return 0;}
+    	return 1;}  
+       
+}
