f62ac2ec3cb34a5f4a923234bd915f5c666b69ff
Stanislas Gorremans <sgorremans@yoda.info.ucl.ac.be>
2019-04-04 11:03:55 +0200

    import io in v1.c, now printf works

--- v1.c
+++ v1.c
@@ -1,7 +1,12 @@
+#include <stdio.h> 
+
 int main( int argc, char*argv[]){
     if(!strcmp(argv[2],"-eq")){
-    	if(atoi(argv[1]) == atoi(argv[3])){return 0;}
-    	return 1;}
+    	if(atoi(argv[1]) == atoi(argv[3])){
+		printf("printf marche!\n");
+		return 0;}
+    	fprintf(stdout,"fprintf marche!\n");
+	return 1;}
     if(!strcmp(argv[2],"-ge")){
     	if(atoi(argv[1]) >= atoi(argv[3])){return 0;}
     	return 1;}       
