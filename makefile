d995a0ba7669b085a17365dbc6c64100e926d3e4
Stanislas Gorremans <sgorremans@yoda.info.ucl.ac.be>
2019-04-06 23:21:18 +0200

    get info form bash much better

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all: src/v1.c
-	gcc -o cracker src/v1.c
+	gcc -o cracker src/v1.c -std=c99
 
 clean:
 	rm cracker
--- src/v1.c
+++ src/v1.c
@@ -17,48 +17,115 @@
 //malloc
 #include <stdlib.h>
 
-// ./cracker [-t NTHREADS] [-c] [-o FICHIEROUT] FICHIER1 [FICHIER2 ... FICHIERN]
+//atoi
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main( int argc, char*argv[]){
-	
+
+	printf("argc = %d \n", argc);	
+	for(int i=0; i<argc; i++){
+	printf("argv[%d] = %s, ",i, argv[i]);}
+	printf("\n");
+
 	//cas aucun d'argument
-	if(argc==0){printf("the function need at least one argument\n");}
+	if(argc==1){
+		printf("the function need at least one argument\n");
+		return -1;
+	}
 	
 	int index = 1;
+	
+	//cas le nombre de thread est preciser
+	char ch[] = "-t"; 
+	if(argc>2 && !strcmp(argv[index], ch)){
+		int nbThread = atoi(argv[index+1]);
+		if(!nbThread){
+			printf("le nombre de thread est incorrect, veillez reessayer\n");
+			return -1;
+		}
+		else{
+			printf("le nombre de thread est de %d\n", nbThread);
+			index=index+2; //car le "-t" et le nombre
+		}
+	}	
 
-	//cas 1 argument
+	//cas des consonnes
+	char ch2[] = "-c";
+	if(argc>index && !strcmp(argv[index], ch2)){
+		index=index+1;
+		printf("cas des consonnes\n");
+	} 
+	//encore à faire! cas ou pas le prochain n'est pas un -o mais l'argument de consonnes n'est pas bien respecter
+
+// ./cracker [-t NTHREADS] [-c] [-o FICHIEROUT] FICHIER1 [FICHIER2 ... FICHIERN]
+
+	//cas d'output dans un document
+	char ch3[] = "-o";
+	//a ajouter la condition pour eviter de prendre comme fichier out en fichier in
+	if(argc>index+1 && !strcmp(argv[index], ch3)){
+		if(1){
+			printf("la sortie du programme se fera dans le document %s\n",argv[index+1]);
+			index=index+2;
+		}
+		else{
+                	printf("le nom fichier de sortie est inexistant, veillez reessayer\n");
+                	return -1;
+        	}
+
+	}
+
+
+	printf("index = %d, argc = %d\n",index, argc);
 	//c'est mieux de créer une méthode pour ouvrir les fichier
-	if(argc==2){
+	
+	/* need to check each file
+	while(index<argc){
+		printf("file\n");
+		index++;
+	}	
+	*/
+
+	//check if there is still something and that the file is a .bin file
+	char ch4[] = ".bin";
+	if(index<argc && strlen(argv[index]) && !strcmp(argv[index]+ strlen(argv[index])-4, ch4)){
 		//open the file
+		printf("reading the file\n");
 		int fp = open(argv[index], O_RDONLY);
-		if(fp == -1){return -1;}
+		if(fp == -1){
+			printf("reading fail");
+			return -1;
+		}
 		
 		//get stat
 		struct stat *buf = malloc(sizeof(struct stat));
 		if(buf==NULL){return -1;}
 		if(stat(argv[1], buf)==-1){return -1;}
 
+		//if help needed look at https://stackoverflow.com/questions/979816/whats-a-binary-file-and-how-do-i-create-one
 		//read the file
-		char* temp= malloc(buf->st_size);
-		if(temp==NULL){return -1;}
-		if(read(fp, temp, buf->st_size)!=buf->st_size){return -1;}
+		char *n1= malloc(sizeof(char));
+		for(int i=0; i*sizeof(char) < buf->st_size; i++){
+			if(read(fp, (int *) n1 +i, sizeof(unsigned char))==-1){return -2;}	
+			printf("%x",n1);
+			if((i+1)%32==0){printf("\n");}
+		}
+
 		
 		//print to check if correctly read
-		printf("%s",temp);
-
+		//printf("temp = %s\n",temp);
 		//free the memory
-		free(temp);
+		//free(temp);
 		free(buf);
 		
 		//close the file
 		if(close(fp)==-1){return -3;}
 	}
-
-/*
-	if(index<argc){
-		printf("is testing\n");
-		if(isdigit(*argv[index])){printf("now need to get this number\n");}	
+	else{
+		printf("please select a file\n");
+		return -1;
 	}
-*/
 
 return 0;
 }
