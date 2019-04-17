de365482d6e43248ddf016589ebcbfd34c49c68e
stanislas <stanislas.gorremans@student.uclouvain.be>
2019-04-17 13:17:54 -0700

    test probleme prod _ cons

---
+++ src/prod_cons.c
@@ -0,0 +1,40 @@
+﻿// Initialisation
+#define N 10 // slots du buffer
+pthread_mutex_t mutex;
+sem_t empty;
+sem_t full;
+
+pthread_mutex_init(&mutex, NULL);
+sem_init(&empty, 0 , N);  // buffer vide
+sem_init(&full, 0 , 0);   // buffer vide
+
+// Producteur
+void producer(void)
+{
+  int item;
+  while(true)
+  {
+    item=produce(item);
+    sem_wait(&empty); // attente d'un slot libre
+    pthread_mutex_lock(&mutex);
+     // section critique
+     insert_item();
+    pthread_mutex_unlock(&mutex);
+    sem_post(&full); // il y a un slot rempli en plus
+  }
+}
+
+// Consommateur
+void consumer(void)
+{
+ int item;
+ while(true)
+ {
+   sem_wait(&full); // attente d'un slot rempli
+   pthread_mutex_lock(&mutex);
+    // section critique
+    item=remove(item);
+   pthread_mutex_unlock(&mutex);
+   sem_post(&empty); // il y a un slot libre en plus
+ }
+}
