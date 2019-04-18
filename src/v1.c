64602a201f0dd4bf45e3fe6b06e5ea2f2b124be4
stanislas <stanislas.gorremans@student.uclouvain.be>
2019-04-17 18:26:30 -0700

    penser en polythread avec v2.c

--- src/v1.c
+++ src/v1.c
@@ -44,7 +44,6 @@ int main( int argc, char*argv[]){
 	int index = 1;
 
 	// case given number of threads
-	// char ch[] = "-t";
 	if(argc>2 && !strcmp(argv[index], "-t")){
 		int nbThread = atoi(argv[index+1]);
 		if(!nbThread){
@@ -95,24 +94,16 @@ int main( int argc, char*argv[]){
 
 	//check if there is still something and that the file is a .bin file
 	// Check length of filenam
-	// char ch4[] = ".bin";
 	if(index<argc && strlen(argv[index])>=4 && !strcmp(argv[index] + strlen(argv[index])-4, ".bin")){
 		//open the file
 		printf("reading the file\n");
 
 		FILE * file = fopen(argv[index], "rb");
-		// int fp = open(argv[index], O_RDONLY);
 		if(!file){
 			printf("reading fail");
 			return -1;
 		}
 
-		//get stat
-		// struct stat *buf = malloc(sizeof(struct stat));
-		// if(buf==NULL){return -1;}
-		// if(stat(argv[1], buf)==-1){return -1;}
-
-		//if help needed look at https://stackoverflow.com/questions/979816/whats-a-binary-file-and-how-do-i-create-one
 		//read the file
 		int sizeread;
 		uint8_t * hash = malloc(sizeof(char)*32);
@@ -124,11 +115,10 @@ int main( int argc, char*argv[]){
 		}
 		do{
 			sizeread = fread(hash, sizeof(uint8_t), 32, file);
+			//print in hex
 			// printf("0x ");
 			// for(int i = 0; i < 32; i++)
-			// {
-			// 	printf("%x ", hash[i]);
-			// }
+			// {printf("%x ", hash[i]); }
 			// printf("\n");
 
 			if(reversehash(hash, resRH, sizeof(char)*16)){
@@ -138,23 +128,6 @@ int main( int argc, char*argv[]){
 		} while(sizeread==32);
 
 
-
-		// char *n1= malloc(sizeof(char));
-		// for(int i=0; i*sizeof(char) < buf->st_size; i++){
-
-		// 	if(read(fp, (int *) n1 +i, sizeof(unsigned char))==-1){return -2;}
-		// 	printf("%x",n1);
-		// 	if((i+1)%32==0){printf("\n");}
-
-			// ssize_t read(int file, void *buf, size_t count);
-			// fread(&val, sizeof(char), 32, )
-
-
-
-
-
-		//print to check if correctly read
-		//printf("temp = %s\n",temp);
 		//free the memory
 		//free(temp);
 
