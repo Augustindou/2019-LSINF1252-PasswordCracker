b080f9b8c9fa2147fd6604a0f5ad2e98847fc59a
stanislas <stanislas.gorremans@student.uclouvain.be>
2019-04-19 11:15:40 -0700

    insert hash in ProdCons

--- src/v2.c
+++ src/v2.c
@@ -12,66 +12,75 @@ int main(int argc, char *argv[]){
 
 	// Initialisation
 	
-	N=10 //à modifier selon le nombre de thread
-	unit8_t *ProdCons[N*32]=malloc(sizeof(unint8_t)*32*N);//create the table
+		N=10 //à modifier selon le nombre de thread
+		unit8_t * ProdCons = (unit8_t *) calloc(N, sizeof(unit8_t)*32);//create the table
 	
-	pthread_mutex_t mutex;
-	sem_t empty;
-	sem_t full;
+		pthread_mutex_t mutex;
+		sem_t empty;
+		sem_t full;
+		//pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 
+		pthread_mutex_init(&mutex, NULL);
+		sem_init(&empty, 0 , N);  // buffer vide
+		sem_init(&full, 0 , 0);   // buffer vide
+		bool finishProd = 0;	  // pas encore la fin du fichier
 
-	pthread_mutex_init(&mutex, NULL);
-	sem_init(&empty, 0 , N);  // buffer vide
-	sem_init(&full, 0 , 0);   // buffer vide
-	bool finishProd = 0;	  // pas encore la fin du fichier
-
-	//ouverture de fichier
-	FILE * file = fopen(argv[index], "rb");
-	if(!file){
-		printf("reading fail");
-		return -1;
-	}
-	//pas oublie de fermer le fichier en cas d'erreur
+		//ouverture de fichier
+		FILE * file = fopen(argv[index], "rb");
+		if(!file){
+			printf("reading fail");
+			return -1;
+		}
+		//pas oublie de fermer le fichier en cas d'erreur
 
 	//le vrai code
 
-	pthread_t threads [N];//thread pour reverseHash
-	//creation des threads consommateurs
+	pthread_t prod;
+	pthread_t cons [N];//thread pour reverseHash
+
+	//creation des threads
+	if(pthread_create(&(prod[i]), NULL, &prod, NULL)){
+			printf("error while creating production thread\n");
+			return -1;
+	}
 	for(int i=0; i<N; i++){
-		if(pthread_create(&(threads[i]), NULL, &consumer, NULL)){
+		if(pthread_create(&(cons[i]), NULL, &consumer, NULL)){
 			printf("error while creating consumer threads\n");
 			return -1;
 		}
 	}
+	
+	//join de threads
+	if(!pthread_join(prod,NULL){
+			printf("error while pthread_join\n");
+			return -1;
+	}
 	for(int i=0; i<N; i++){
-		char* resRH;
-		if(!pthread_join(threads[i], (void **) &resRH)){
+		if(!pthread_join(cons[i],NULL){
 			printf("error while pthread_join\n");
 			return -1;
 		}//check errors
-		printf("%s",resRH);
-		free(resRH);
 	}
 
 	//terminasion
-	free(ProdCons);
-	sem_destroy(empty);
-	sem_destroy(full);
+		free(ProdCons);
+		pthread_mutex_destroy(mutex);
+		sem_destroy(empty);
+		sem_destroy(full);
 
 	return 0;
 }
 
 //readFile for threads
-unint8_t* readBinFile(FILE* file)
-{
+unint8_t* readBinFile(FILE* file){
 	uint8_t * hash = malloc(sizeof(char)*32);
-	if(!hash || !resRH){
-		free(hash); free(resRH);
+	if(!hash){
+		free(hash);
 		printf("malloc fail\n");
 		return -1;
 	}//malloc errors
 
 	if(fread(hash, sizeof(uint8_t), 32, file)==32){
-		return hash;
+		return (unit8_t *) hash;
 	}//read file 
 	else{
 		finishProd=1;
@@ -80,31 +89,29 @@ unint8_t* readBinFile(FILE* file)
 	}//end of the file
 
 	return hash;
-}//return char* with password
+}//return unint8_t* with hash
 
 // Producteur, Hash
-void * producer(FILE* file)
-{
+void * producer(FILE* file){
 	unint8_t* hash;
 	while(!finishProd)
 	{
-	hash=readBinFile(file);	//readf() 
-	//met finishProd à 1 à la fin en fermant le fichier
-	sem_wait(&empty); // attente d'un slot libre
-	pthread_mutex_lock(&mutex);
-		// section critique
-		insertHash(hash, tab, N);	//ajout dans le tableau la chaine de 32 byte
-	pthread_mutex_unlock(&mutex);
-	sem_post(&full); // il y a un slot rempli en plus
+		hash=readBinFile(file);	//readf() 
+		
+		sem_wait(&empty); // attente d'un slot libre
+		pthread_mutex_lock(&mutex);
+			// section critique
+			insertHash(hash, ProdCons, N);	//ajout dans le tableau la chaine de 32 byte
+		pthread_mutex_unlock(&mutex);
+		sem_post(&full); // il y a un slot rempli en plus
 	}
 
 	return (NULL);
 }
 
 // Consommateur, reverseHash
-void * consumer(void)
-{
-	char* hash; //tableau de 32 byte (voir Producteur)
+void * consumer(void){
+	unint8_t* hash; //tableau de 32 byte (voir Producteur)
 	char * resRH = malloc(sizeof(char)*16);
 	while(!finishProd || getSemValue(&full) )	//check si la production est terminée & vérifie si le tableau est vide 
 	{
@@ -124,8 +131,7 @@ void * consumer(void)
 }
 
 //permet d'obtenir la valuer du semaphore passer en argument
-int getSemValue(sem_t * sem)
-{
+int getSemValue(sem_t * sem){
 	int value;
 	sem_getvalue(&sem, &value);
 	return value;
@@ -134,11 +140,23 @@ int getSemValue(sem_t * sem)
 //////////////////////////////////////////////////////////////////////
 //encore à implementer
 
-void insertHash(char* resRH, unit8_t *ProdCons[], N){
-	for(int i=0,i<N,i++;
-	if(ProdCons[i]==0){ProdCons[i]}
+void insertHash(unint8_t* resRH, unit8_t *ProdCons, N){
+	int counter=0;
+	for(int i=0; i<N & !counter; i++){
+	counter=0;
+		//check if the 32 byte equal zero
+		for(int j=0; j<32; j++){
+			counter = counter + *(ProdCons+i*32+j);
+		}
+		if (counter==0){
+			for(int j=0; j<32; j++){
+				*(ProdCons+j)=*(resRH+j);
+			}
+		}
+	}
+	return NULL;
 }
 
 void removeHash(unit8_t *ProdCons[], N){
-//ne pas oublie de remettre la valuer à zéro dans le tableau après de l'avoir extraite
+	//ne pas oublie de remettre la valuer à zéro dans le tableau après de l'avoir extraite
 }
