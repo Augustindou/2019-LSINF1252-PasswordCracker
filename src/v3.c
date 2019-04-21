992efdfa11cc351b189faa0110e20ecfb6e560ad
stanislas <stanislas.gorremans@student.uclouvain.be>
2019-04-21 11:39:44 -0700

    to smaller cons[i] ter

--- src/v3.c
+++ src/v3.c
@@ -162,13 +162,13 @@ int main(int argc, char *argv[]){
 		printf("error while prod pthread_join\n");
 		return -1;
 	}
-	for(int i=N-1; i>0; i--){
+	for(int i=N-1; i>=0; i--){
 		if(pthread_join(cons[i],NULL)!=0){
 			printf("error while cons[%d] pthread_join\n",i);
 			return -1;
 		}//check errors
 		printf("fin de cons[%d]\n", i);
-		if(i>=N-1){
+		if(i==0){
 			finishProd2=1;
 			printf("cons done, finishProd2: %d", finishProd2);
 		}
