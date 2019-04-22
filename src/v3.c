5747c75f2126cb9edbc1cb037cb1bc52311654d1
stanislas <stanislas.gorremans@student.uclouvain.be>
2019-04-21 22:31:14 -0700

    try to fix deadend in sort 3

--- src/v3.c
+++ src/v3.c
@@ -313,7 +313,7 @@ void * sort(){
 		if(finishProd2){printf("sort, finishProd2: %d\n", finishProd2);}
 	}
 
-	printf("End sort, full2: %d, empty2: %d\n", getSemValue(&full2),getSemValue(&empty2));
+	printf("End sort, full2: %d, empty2: %d\n");
 	return NULL;
 }
 
