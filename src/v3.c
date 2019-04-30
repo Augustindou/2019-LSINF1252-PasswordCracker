9df4b976acff3aebb4f3c0b5f481a505ebf5b47c
stanislasg <stanislas.gorremans@student.uclouvain.be>
2019-05-01 00:02:08 +0200

    working program

--- src/v3.c
+++ src/v3.c
@@ -11,6 +11,9 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <getopt.h>
+#include <time.h>
+
+
 
 // structure
 struct node {
@@ -33,12 +36,16 @@ struct node {
   int printStack(struct node **head);
   int strlenVo(char* candidat, bool consonant);
   int saveToFile(struct node ** head, FILE * OutputFile);
+  bool sortCond();
 
 //variables
+  // define a la place de sizeofHash et sizeofString
+  int sizeofHash = 32;
+  int sizeofString = 17; // 16 + '\0'
   FILE* file;
   FILE* outFile;
   bool OutputToFile = false;
-  int N;
+  int N = 1;
   //prodCons1
   uint8_t * ProdCons;
   pthread_mutex_t mutex;
@@ -53,13 +60,17 @@ struct node {
   int finishProd2;
   int finishCons;
   bool consonne = false;
+  //CondSort
+  pthread_mutex_t mutex3;
+  int consFinish = 0;
 
   struct node * head;
 
 
 
 int main(int argc, char *argv[]){
-    //définir les les variables avec argv[], comme dans v1.c
+    time_t start =time(0);
+    //definir les les variables avec argv[], comme dans v1.c
     printf("argc = %d \n", argc);
     for(int i=0; i<argc; i++){
     printf("argv[%d] = %s, ",i, argv[i]);}
@@ -103,12 +114,12 @@ int main(int argc, char *argv[]){
 
   // Initialisation
 
-    ProdCons = (uint8_t *) calloc(N, sizeof(uint8_t)*32);//create the table
+    ProdCons = (uint8_t *) calloc(N, sizeof(uint8_t)*sizeofHash);//create the table
     if(ProdCons==NULL){
       printf("calloc ProdCons fail\n");
       return -1;
     }
-    ProdCons2 = (char *) calloc(N, sizeof(char)*17);//max 16 lettres long, mieux de definir une variables au debut
+    ProdCons2 = (char *) calloc(N, sizeof(char)*sizeofString);
     if(ProdCons==NULL){
       printf("calloc ProdCons2 fail\n");
       return -1;
@@ -126,6 +137,8 @@ int main(int argc, char *argv[]){
     sem_init(&empty2, 0 , N);  // buffer vide
     sem_init(&full2, 0 , 0);   // buffer vide
     finishProd2 = 0;    // pas encore la fin du bruteforce
+
+    pthread_mutex_init(&mutex3, NULL);
 
     //ouverture de fichier
     file = fopen(argv[optind], "rb");
@@ -206,12 +219,15 @@ int main(int argc, char *argv[]){
     sem_destroy(&empty2);
     sem_destroy(&full2);
 
+    time_t end =time(0);
+    double TheTime = difftime(end, start);//Copyright Ben Deloigne
+    printf("program end in %f second\n",TheTime);
   return 0;
 }
 
 //readFile for threads
 uint8_t* readBinFile(FILE* file, uint8_t * hash){
-  if(fread(hash, sizeof(uint8_t), 32, file)==32){
+  if(fread(hash, sizeof(uint8_t), sizeofHash, file)==sizeofHash){
     return hash;
   }//read file
   else{
@@ -222,14 +238,14 @@ uint8_t* readBinFile(FILE* file, uint8_t * hash){
       return NULL;
     }
     //attention double le dernier hash trouver une methode pour eviter ca!!
-  }//end of the file
+  } //end of the file
 
   return hash;
 }//return unint8_t* with hash
 
 // Producteur, Hash
 void * producer(){
-  uint8_t * hash = malloc(sizeof(char)*32);
+  uint8_t * hash = malloc(sizeof(char)*sizeofHash);
   if(!hash){
     printf("malloc fail\n");
     return NULL;
@@ -242,7 +258,7 @@ void * producer(){
     sem_wait(&empty); // attente d'un slot libre
     pthread_mutex_lock(&mutex);
       // section critique 1
-      insertHash(hash, ProdCons, N);  //ajout dans le tableau la chaine de 32 byte
+      insertHash(hash, ProdCons, N);  //ajout dans le tableau la chaine de sizeofHash (32) byte
     pthread_mutex_unlock(&mutex);
     sem_post(&full); // il y a un slot rempli en plus
   }
@@ -254,15 +270,18 @@ void * producer(){
 
 // Consommateur, reverseHash
 void * consumer(){
-  uint8_t * hash = malloc(sizeof(char)*32);
-  char * resRH = malloc(sizeof(char)*16);//16 ou 17?, definir au debut!
+  uint8_t * hash = malloc(sizeof(char)*sizeofHash);
+  char * resRH = malloc(sizeof(char)*sizeofString);
   if(!hash || !resRH){
     free(hash); free(resRH);
     printf("malloc fail\n");
     return NULL;
   }
-  while(!finishProd || getSemValue(&full) ) //check si la production est terminee et vérifie si le tableau est vide
+
+  pthread_mutex_lock(&mutex3);
+  while(!finishProd || getSemValue(&full) ) //check si la production est terminee et verifie si le tableau est vide
   {
+    pthread_mutex_unlock(&mutex3);
     sem_wait(&full); // attente d'un slot rempli
     pthread_mutex_lock(&mutex);
       // section critique 1
@@ -270,11 +289,12 @@ void * consumer(){
     pthread_mutex_unlock(&mutex);
     sem_post(&empty); // il y a un slot libre en plus
 
-    if(!reversehash(hash, resRH, sizeof(char)*16)){//16 ou 17?, definir au debut!
+    if(!reversehash(hash, resRH, sizeof(char)*sizeofString)){
       printf("petite erreur dans reverseHash!\n");
     }//le mot de passe ne respecte pas les consignes
 
     sem_wait(&empty2); // attente d'un slot libre
+    pthread_mutex_lock(&mutex3);
     pthread_mutex_lock(&mutex2);
       // section critique 2
       insertResRH(resRH, ProdCons2, N);
@@ -282,28 +302,32 @@ void * consumer(){
     sem_post(&full2); // il y a un slot rempli en plus
 
   }
-  finishCons++;
+  consFinish++;
+  printf("End consumer before closing mutex3\n");
+  pthread_mutex_unlock(&mutex3);
   //printf("End consumer, full: %d, empty: %d\n", getSemValue(&full),getSemValue(&empty));
-  printf("End consumer, full2: %d, empty2: %d, finishProd2:%d\n", getSemValue(&full2),getSemValue(&empty2), finishProd2);
+  printf("End consumer, full2: %d, empty2: %d\n", getSemValue(&full2),getSemValue(&empty2));
 
   free(hash);
   return NULL;
 }
 
 void * sort(){
-  char * resRH = malloc(sizeof(char)*16);//16 ou 17?, definir au debut!
+  char * resRH = malloc(sizeof(char)*sizeofString);
   if(!resRH) {
     printf("malloc fail\n");
     return NULL;
   }
-  while(finishCons==0 || getSemValue(&full2) )  //check si la production est terminee et vérifie si le tableau est vide
+  while(sortCond())  //check si la production est terminee et verifie si le tableau est vide
   {
+    printf("sortWhile\n");
     // printf("sort, finishCons: %d, full2:%d\n", finishProd2, getSemValue(&full2));
     //if(!getSemValue(&full2)){printf("sort, full2: %d\n", getSemValue(&full2));}
     sem_wait(&full2); // attente d'un slot rempli
     pthread_mutex_lock(&mutex2);
       // section critique 2
       removeResRH(resRH, ProdCons2, N);
+      printf("mot: %s\n", resRH);
     pthread_mutex_unlock(&mutex2);
     sem_post(&empty2); // il y a un slot libre en plus
     if(head==NULL){
@@ -331,7 +355,7 @@ void * sort(){
   return NULL;
 }
 
-//permet d'obtenir la valeur du semaphore passé en argument
+//permet d'obtenir la valeur du semaphore passe en argument
 int getSemValue(sem_t * sem){
   int value;
   sem_getvalue(sem, &value);
@@ -343,12 +367,12 @@ void insertHash(uint8_t * hash, uint8_t *ProdCons, int N){
   int counter;
   for(int i=0; i<N; i++){
     counter=0;
-    for(int j=0; j<32; j++){
-      counter = counter + *(ProdCons+i*32+j);
+    for(int j=0; j<sizeofHash; j++){
+      counter = counter + *(ProdCons+i*sizeofHash+j);
     }
     if (!counter){
-      for(int j=0; j<32; j++){
-        *(ProdCons+i*32+j)=*(hash+j);
+      for(int j=0; j<sizeofHash; j++){
+        *(ProdCons+i*sizeofHash+j)=*(hash+j);
       }
       return;
     }
@@ -360,13 +384,13 @@ void removeHash(uint8_t* hash, uint8_t *ProdCons, int N){
   int counter=0;
   for(int i=0; i<N & !counter; i++){
     counter=0;
-    for(int j=0; j<32; j++){
-      counter = counter + *(ProdCons+i*32+j);
+    for(int j=0; j<sizeofHash; j++){
+      counter = counter + *(ProdCons+i*sizeofHash+j);
     }
     if (counter){
-      for(int j=0; j<32; j++){
-        *(hash+j)=*(ProdCons+i*32+j);
-          *(ProdCons+i*32+j)=0;
+      for(int j=0; j<sizeofHash; j++){
+        *(hash+j)=*(ProdCons+i*sizeofHash+j);
+          *(ProdCons+i*sizeofHash+j)=0;
       }
     }
   }
@@ -376,7 +400,7 @@ void removeHash(uint8_t* hash, uint8_t *ProdCons, int N){
 //peut etre combiner avec le precedant avec une variables supplementaire, taille de chaque element
 void insertResRH(char * resRH, char *ProdCons2, int N){
   int counter;
-  int nbLettre=16;//16 ou 17?, definir au debut!
+  int nbLettre=sizeofString;
   for(int i=0; i<N; i++){
     counter=0;
     for(int j=0; j<1; j++){
@@ -394,7 +418,7 @@ void insertResRH(char * resRH, char *ProdCons2, int N){
 
 void removeResRH(char * resRH, char *ProdCons2, int N){
   int counter=0;
-  int nbLettre=16;//16 ou 17?, definir au debut!
+  int nbLettre=sizeofString;
   for(int i=0; i<N & !counter; i++){
     counter=0;
     for(int j=0; j<nbLettre; j++){
@@ -439,12 +463,11 @@ int push(struct node **head, const char *value){
 *
 * @head : pointer to the top of the stack
 *
-* @return 0 if no error, -1 otherwise
+* @return 0 if no error, -1 otherwise // enft y a pas moy de detecter des erreurs pck free() est une fct void... donc en soi autant mettre une void pop(struct node** head);
 *
 * pre :
 * post :
 */
-
 int pop(struct node **head){
   while(*head){
     struct node * first = *head;
@@ -494,4 +517,25 @@ int saveToFile(struct node ** head, FILE * OutputFile){
     printf("No writing errors !\n");
   }
   return 0;
+}
+
+// return true if sort should continue sorting ; false if not
+bool sortCond(){
+  if(getSemValue(&empty2) != N){
+    printf("SV\n");
+    return true;
+  }
+  else if(pthread_mutex_trylock(&mutex3) == 0) {
+    pthread_mutex_unlock(&mutex3);
+    printf("TL, consFinish = %d\n",consFinish);
+    if(consFinish >= N){//changed
+      return false;
+    }
+    return true;
+  }
+  else
+  {
+    sleep(1);
+    return sortCond();
+  }
 }
