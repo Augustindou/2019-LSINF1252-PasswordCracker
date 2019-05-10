da4595a54f2b25afede3b4b47bf9b41615a0d6db
augustindou <augustin.doultremontao@gmail.com>
2019-05-10 12:01:28 +0200

    bashTimes changed

--- bashTimes.sh
+++ bashTimes.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+rm results.txt
 touch results.txt
-for i in {1..40}
+for j in {1..3}
 do
-  start=`date +%s`
-  ./cracker -t $i -c -o out.txt tests/test-input/01_4c_1k.bin
-  end=`date +%s`
-  runtime=$((end-start))
-  echo "$i : $runtime [s]" >> results.txt
+  echo "---------------------------------------------------" >> results.txt
+  echo "Test : $j" >> results.txt
+  echo "---------------------------------------------------" >> results.txt
+  for i in {1..40}
+  do
+    start=`date +%s`
+    ./cracker -t $i -c -o out.txt tests/test-input/01_4c_1k.bin
+    end=`date +%s`
+    runtime=$((end-start))
+    echo "$i : $runtime" >> results.txt
+  done
 done
