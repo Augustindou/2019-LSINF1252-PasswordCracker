32f6a7d522e3cf7c19e758d1e1c1ad83b8034a9e
augustindou <augustin.doultremontao@gmail.com>
2019-05-10 14:05:28 +0200

    random

--- src/v3Func.c
+++ src/v3Func.c
@@ -144,7 +144,7 @@ void * sort(){
     if(err!=0){intError(err, "pthread_mutex_lock mutex2 error");}
       // critical zone 2
       removeFromBuffer(resRH, ProdCons2, N, true);
-      // printf("mot: %s\n", resRH);
+      //printf("mot: %s\n", resRH);
     err = pthread_mutex_unlock(&mutex2);
     if(err!=0){intError(err, "pthread_mutex_unlock mutex2 error");}
     err = sem_post(&empty2); // a slot has been cleaned
